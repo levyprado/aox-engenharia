4960de7763c5bd74ce8807cddbbb158e412bf5b5
Levy Prado <levyprado28@gmail.com>
2026-03-01 15:47:55 -0400

    feat: integrate services section into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/layout/header'
 import Hero from '@/components/sections/hero'
+import Services from '@/components/sections/services'
 
 export default function HomePage() {
   return (
@@ -7,6 +8,7 @@ export default function HomePage() {
       <Header />
       <main>
         <Hero id='inicio' />
+        <Services id='servicos' />
       </main>
       <footer></footer>
     </>
